353a0187d8b0414ac08095c5fcda55e353e0871b
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-06-04 14:59:36 +0100

    Added stipe elements and style

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51GqJ5EHKfLOwRnJ3XFXbQ8xlzVlnViREL22CaqdLVUKpK4noXLjrl6HawbOUuIXQUZw3gg1Ib8pPuvIqQk4Pag1D00mheCMCdh',
+        'client_secret': 'test_user',
     }
 
     return render(request, template, context)
